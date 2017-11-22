480e668592e2f881a23b48f4c10108bc04029da5
Mojiashen1 <mojiashen1@gmail.com>
2017-11-22 17:44:14 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+# Mojia & Harshita
+# final project
+
+from flask import Flask, render_template, request, flash, redirect, url_for
+import os, sys
+import MySQLdb
+import movie as movie
+import dbconn2
+
+app = Flask(__name__)
+
+@app.route('/', methods =['POST', 'GET'])
+def home():
+    return render_template('scott.html')
+
+@app.route('/login/', methods =['POST', 'GET'])
+def login():
+    pass
+
+app.secret_key = 'youcantguessthisout'
+
+if __name__ == '__main__':
+  app.debug == True
+  port = os.getuid()
+  app.run('0.0.0.0', port)
---
+++ helper.py
@@ -0,0 +1,13 @@
+# Mojia & Harshita
+# final project
+
+import sys
+import MySQLdb
+import dbconn2
+from flask import flash
+
+#Connects to the db
+def getConn():
+    DSN = dbconn2.read_cnf()
+    DSN['db'] = 'mshen2_db'
+    return dbconn2.connect(DSN)
