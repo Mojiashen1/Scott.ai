9843a80f47650d48b750ecb7ef5dcb1534172897
Harshita Yerramreddy <harshita@Harshitas-MacBook-Pro.local>
2017-11-26 20:01:19 -0500

    fix login

--- app.py
+++ app.py
@@ -25,6 +25,10 @@ def signup():
     name = request.form['name']
     username = request.form['email']
     password = request.form['password']
+
+    print (name)
+    print (username)
+    print (password)
     desc = helper.create_account(name, username, password)
     return redirect(url_for('signup'))
 
