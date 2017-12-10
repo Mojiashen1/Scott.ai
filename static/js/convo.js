f6d1d5a4a869553a6e5f7d3f432db43ed79e3578
Mojiashen1 <mojiashen1@gmail.com>
2017-12-10 17:05:36 -0500

    asd

--- static/js/convo.js
+++ static/js/convo.js
@@ -2,5 +2,6 @@ var index = 0;
 $('#next').click(function(e) {
   index++;
   var questions = $('h4').attr('data-question')
+  console.log(questions)
   $('h4').val(questions[index]['questionText'])
 })
