20f5dffc6f72aedf40845935659e1ec9f39248bd
Harshita Yerramreddy <harshita@Harshitas-MacBook-Pro.local>
2017-12-13 21:46:07 -0500

    save audio

--- app.py
+++ app.py
@@ -271,6 +271,14 @@ def progress():
 
           if request.form['submit'] == 'delete':
             delete_audio(userId) #delete using convo primary key
+  
+            #re render template
+            points = get_user_data(userId)
+            data = get_convos(userId)
+            
+            return render_template('progress.html', 
+            points=points['points'], 
+            data=data, script=url_for('progress'))
 
         return render_template('progress.html', 
           points=points['points'], 
