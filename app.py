6f0927fcd623316681a15663a35fc92f5fee43d7
Mojiashen1 <mojiashen1@gmail.com>
2017-12-16 11:21:36 -0500

    working on file upload

--- app.py
+++ app.py
@@ -217,7 +217,7 @@ def convo(categoryId):
       else:
             print('inside convos post')
             audio_length = 1 # minutes of the new audio
-            print('get things out of file name', request.files['blob'].filename)
+            print('get things out of file name', request.files['blob'].file)
             file = request.files['blob']
             # convoId = request.form['convoId']
             feedback = create_feedback(userId, file)
