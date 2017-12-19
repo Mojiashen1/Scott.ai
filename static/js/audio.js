5a7c5ef1429b16d2bd9b8f2e6a80b8befa947390
Harshita Yerramreddy <harshita@Harshitas-MacBook-Pro.local>
2017-12-19 11:56:30 -0500

    final

--- static/js/audio.js
+++ static/js/audio.js
@@ -148,7 +148,6 @@ stopRecordingButton.addEventListener("click", function () {
         },
       complete: function(f){
             console.log("ajax complete");
-            $('#response').html('Audio saved!')
             //redirect to feedback page within complete instead
             window.location.href = redirectLink;
             return 1;
